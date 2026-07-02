165217bff11c09e0986fb8a12bb1f184f3352a5c
dev_leo <146463507+lxzzt@users.noreply.github.com>
2026-07-02 18:23:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="divPai">
+      <div className="divConteudo">
+        <h1>Montanha Burguer</h1>
+      </div>
+    <div className="div3d">
+    </div>
+    </div>
+  )
+}
+
+export default App
