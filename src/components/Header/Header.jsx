506bf6b61ca796a92c75aa697ae03f243c0e9e22
dev_leo <146463507+lxzzt@users.noreply.github.com>
2026-07-20 19:40:49 -0300

    v23c
    Fix:em celular agora os card estão responsivos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,9 +17,9 @@ const Header = () => {
                 {/* Centro: Menu */}
                 <nav className={styles.navMenu}>
                     <a href="#home" className={styles.navLink}>Início</a>
-                    <a href="#cardapio" className={styles.navLink}>Cardápio</a>
-                    <a href="#quests" className={styles.navLink}>Quests & Eventos</a>
                     <a href="#fortalezas" className={styles.navLink}>Fortalezas</a>
+                    <a href="#quests" className={styles.navLink}>Quests & Eventos</a>
+                    <a href="#cardapio" className={styles.navLink}>Cardápio</a>
                 </nav>
 
                 {/* Direita */}
