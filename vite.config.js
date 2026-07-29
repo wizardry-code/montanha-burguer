16e3119855607c5ce70f16fab17c5ad0fb1fbc73
dev_leo <146463507+lxzzt@users.noreply.github.com>
2026-07-28 22:07:08 -0300

    v47
    Fix:nota pc 91+ e nota cll 60+.eu fiz o thee-shaking para otimização na importação de bibliotecas

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,27 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-        visualizer({ open: true, gzipSize: true, brotliSize: true, filename: 'stats.html' })
-,
+    visualizer({ open: true, gzipSize: true, brotliSize: true, filename: 'stats.html' }),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
   base: '/montanha-burguer/',
+  build: {
+    rollupOptions: {
+      output: {
+        // Separa as grandes dependências em arquivos isolados para otimizar o carregamento
+        manualChunks(id) {
+          if (id.includes('node_modules/three') || id.includes('node_modules/@react-three')) {
+            return 'vendor-three';
+          }
+          if (id.includes('node_modules/gsap')) {
+            return 'vendor-gsap';
+          }
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor-react';
+          }
+        }
+      }
+    }
+  }
 })
